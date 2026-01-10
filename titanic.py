5d16c04fe6f264001329904a516e93c665fe7087
Marwan <marwan.burhani@hotmail.com>
2026-01-09 20:21:05 -0700

    Initial Work

--- titanic.py
+++ titanic.py
@@ -20,8 +20,31 @@ from sklearn.pipeline import Pipeline, make_pipeline
 test_set = pd.read_csv('data_kaggle/test.csv')
 train_set = pd.read_csv('data_kaggle/train.csv')
 
+
+features = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+
+
+
+X = train_set[features].copy()
+y= train_set['Surived'].copy()
+
+#print(X.head(10))
+
+
 #print(train_set.info())
-print(train_set.Cabin)
+#print(train_set.describe())
+#print(train_set.Cabin)
+
+correlation= train_set.corr(numeric_only=True)
+print(correlation['Survived'].sort_values(ascending=False))
+#Fare had a very strong correlation with surival - Crucial feature, so did Pclass (strong negative correlation)
+
+
+
+print(train_set.Name.head(20)) #Some names have Master, some named do not... Indicator? Could length of name also tell us about status?, Lets see...
+
+
+
 # every feature has 891 except age and cabin. lets look into those
 features = ('PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
        'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked')
