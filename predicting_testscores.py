dab86f6c2e7a10680fecde7d8fb7c9d4a6b79b84
Marwan <marwan.burhani@hotmail.com>
2026-01-19 21:54:09 -0700

    interaction terms

--- predicting_testscores.py
+++ predicting_testscores.py
@@ -8,7 +8,7 @@ import urllib.request
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, GridSearchCV, RandomizedSearchCV
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler, FunctionTransformer
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler, FunctionTransformer, PolynomialFeatures
 from sklearn.compose import TransformedTargetRegressor, ColumnTransformer
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.compose import make_column_selector, make_column_transformer
@@ -27,7 +27,47 @@ test = pd.read_csv('data_playground/test.csv')
 
 print(train.info())
 
+'''
+ #   Column            Non-Null Count   Dtype  
+---  ------            --------------   -----  
+ 0   id                630000 non-null  int64  
+ 1   age               630000 non-null  int64  
+ 2   gender            630000 non-null  object 
+ 3   course            630000 non-null  object
+ 4   study_hours       630000 non-null  float64
+ 5   class_attendance  630000 non-null  float64
+ 6   internet_access   630000 non-null  object
+ 7   sleep_hours       630000 non-null  float64
+ 8   sleep_quality     630000 non-null  object
+ 9   study_method      630000 non-null  object
+ 10  facility_rating   630000 non-null  object
+ 11  exam_difficulty   630000 non-null  object
+ 12  exam_score        630000 non-null  float64
+'''
 
-features = [['id', 'age', 'gender', 'course', 'study_hours', 'class_attendance',
+features = [ 'age', 'gender', 'course', 'study_hours', 'class_attendance',
        'internet_access', 'sleep_hours', 'sleep_quality', 'study_method', 
-       'facility_rating', 'exam_difficulty', 'exam_score']]
+       'facility_rating', 'exam_difficulty']
+
+label = 'exam_score'
+
+cat_features = ['gender','course','exam_difficulty','internet_access','sleep_quality']
+
+X = train[features]
+y = train[label]
+ids = train['id']
+
+
+
+
+'''FEATURE ENGINEERING'''
+#Interactions terms = Sleep_hours x Sleep_quality ... study_hours x internet access .... class attendance x course
+
+print(X['sleep_quality'].value_counts())
+
+
+'''poly = PolynomialFeatures(degree=2, interaction_only=True,include_bias=False)
+
+interactions = poly.fit_transform(X[['sleep_q_enc','sleep_hours']])
+
+print(interactions)'''
