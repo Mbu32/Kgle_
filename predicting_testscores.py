268381d9a3d2506b79f094a3a501c9a80af42cf2
Marwan <marwan.burhani@hotmail.com>
2026-01-25 19:44:58 -0700

    fixed up CatB

--- predicting_testscores.py
+++ predicting_testscores.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import KNeighborsRegressor
 from pathlib import Path
 import tarfile
 import urllib.request
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler, FunctionTransformer, PolynomialFeatures
 from sklearn.compose import TransformedTargetRegressor, ColumnTransformer, make_column_selector, make_column_transformer
@@ -14,12 +13,13 @@ from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.metrics import root_mean_squared_error
 from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
 from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.svm import LinearSVR, SVR
 from sklearn.ensemble import VotingRegressor, StackingRegressor, RandomForestRegressor, ExtraTreesRegressor,BaggingRegressor
 from xgboost import XGBRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
+from sklearn.svm import LinearSVR, SVR
 from sklearn.inspection import permutation_importance
 from lightgbm import LGBMRegressor
 from catboost import CatBoostRegressor
@@ -64,7 +64,7 @@ X = train[features].copy()
 y = train[label].copy()
 ids = train['id'].copy()
 
-X_train, X_val , y_train, y_val = train_test_split(X,y,test_size=.2,random_state=42,stratify= y)
+X_train, X_val , y_train, y_val = train_test_split(X,y,test_size=.2,random_state=42)
 
 
 
@@ -160,58 +160,13 @@ preprocessing_tree = ColumnTransformer([
     ('preprocessing',preprocessing_tree),
     ('model_rf', RandomForestRegressor(random_state=42))
 ]
-)"""
+)
 light_pipeline = Pipeline([
     ('preprocessing',preprocessing_tree),
     ('model_lg', LGBMRegressor(random_state=42,verbose=-1))
 ]
-)
-
-catboost = CatBoostRegressor(random_state=42,verbose=0,cat_features=['gender','course','exam_difficulty',
-                                                          'study_method','facility_rating',
-                                                          'sleep_quality','internet_access'])
-
-
-
-mapping_catboost = {
-    'iterations': [100, 200, 300, 500],
-    'learning_rate': [0.01, 0.03, 0.05, 0.1, 0.2],
-    'depth': [4, 6, 8, 10],
-    'l2_leaf_reg': [1, 3, 5, 7, 9],
-    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
-    'colsample_bylevel': [0.6, 0.7, 0.8, 0.9, 1.0],
-    'min_data_in_leaf': [1, 3, 5, 10, 20]
-}
-
-
-search = RandomizedSearchCV(estimator=catboost,
-                            param_distributions=mapping_catboost,
-                            n_iter=15,
-                            cv=3,scoring='neg_root_mean_squared_error',
-                            verbose=1,
-                            random_state=42,
-                            error_score='raise',
-                            return_train_score=True
-                            )
-
-search.fit(X_train,y_train)
-
-best_model = search.best_estimator_
-best_params = search.best_params_
-score_tree = search.score(X_val,y_val)
-
-y_val_pred = best_model.predict(X_val)
-score_rmse = root_mean_squared_error(y_val, y_val_pred)
-
-print(f"Best parameters: {best_params}")
-print(f"Validation RMSE: {score_rmse:.3f}")
-print(f"Best CV RMSE during search: {-search.best_score_:.3f}")
-
-
-
-
-
-
+)"""
+#inital check on Tree Models CV RMSE:
 '''
 cv_evaluator = RegressionCV(n_splits=3,shuffle=True,random_state=42)
 rf_scores = cv_evaluator.evaluate(model=rf_pipeline,X=X_train,y=y_train)
@@ -231,7 +186,50 @@ Randomforest CV RMSE: 9.726 ± 0.018
 
 
 
-#RandomSearchCV 
+#CatBoost Randomized_search
+"""
+catboost = CatBoostRegressor(random_state=42,verbose=0,loss_function='RMSE',eval_metric='RMSE',cat_features=['gender','course','exam_difficulty',
+                                                          'study_method','facility_rating',
+                                                          'sleep_quality','internet_access'])
+
+
+
+mapping_catboost = {
+    'iterations': [100, 200, 300, 500],
+    'learning_rate': [0.01, 0.03, 0.05, 0.1, 0.2],
+    'depth': [4, 6, 8, 10],
+    'l2_leaf_reg': [1, 3, 5, 7, 9],
+    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
+    'colsample_bylevel': [0.6, 0.7, 0.8, 0.9, 1.0],
+    'min_data_in_leaf': [1, 3, 5, 10, 20]
+}
+
+
+search = catboost.randomized_search(param_distributions=mapping_catboost,X=X_train,y=y_train,cv=3,
+                                    n_iter=15,
+                                    partition_random_seed=42,
+                                    calc_cv_statistics=True,
+                                    search_by_train_test_split=True,
+                                    refit=True,shuffle=True,stratified=None,
+                                    train_size=.8,
+                                    verbose=True    
+)
+
+
+for param in mapping_catboost.keys():
+    print(f"{param}: {catboost.get_param(param)}")
+
+best_params = search['params']
+
+print("Best params:", best_params)
+"""
+
+
+
+
+
+
+#RandomSearchCV for XGBoost
 
 """mapping_xgb = {'model__learning_rate':np.logspace(-2,-.5,10),
                'model__max_depth':[2,3,4,5,6],
@@ -281,9 +279,108 @@ final_model = XGBRegressor(learning_rate=0.046415888336127774,n_estimators= 1200
  
  score: -9.383293361587777
 
-
 '''
-#RandomForest
+
+
+
+
+
+
+
+
+
+
+# LINEAR MODELS PLUS THEIR PIPELINE + Distribution Check, anything else? Well who knows my friend, who knows
+
+#starting with distribution check
+
+
+
+'''Pipelines for linear models'''
+"""
+def internet_interaction(X):
+    study_hours = X[:,1].astype(float)
+    internet_encoded = np.where(X[:,0]=='yes' ,2,1)
+    return((internet_encoded*study_hours).reshape(-1,1))
+
+def sleep_interaction(X):
+    sleep_q_map = {'poor':1,'average':2,'good':3}
+    sleep_q= np.vectorize(sleep_q_map.get)(X[:,0])
+    sleep_hours = X[:,1].astype(float)
+    return(sleep_q*sleep_hours).reshape(-1,1)
+
+
+def internet_hours_name(function_transformer,feature_names_in):
+    return['Internetxstudy_hours']
+
+
+def sleep_quality_name(function_transformer,feature_names_in):
+    return['sleep_q_int']
+
+
+def internet_interaction_pipeline():
+    return(make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        FunctionTransformer(internet_interaction,validate=False,
+                            feature_names_out=internet_hours_name),
+        StandardScaler())
+)
+
+
+def sleep_interaction_pipeline():
+    return (make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        FunctionTransformer(sleep_interaction,validate=False,
+                            feature_names_out=sleep_quality_name),
+        StandardScaler())
+)
+
+
+cat_pipeline = make_pipeline(
+    SimpleImputer(strategy='most_frequent'),
+    OneHotEncoder(handle_unknown='ignore'),
+    StandardScaler(with_mean=False)
+)
+
+
+default_num_pipeline = make_pipeline(
+    SimpleImputer(strategy='median'),
+    StandardScaler()
+)
+
+
+preprocessing_linear_models = ColumnTransformer([
+        ('Internetxstudy_hours',internet_interaction_pipeline(),['internet_access','study_hours']),
+        ('sleep_q_int',sleep_interaction_pipeline(),['sleep_quality','sleep_hours']),
+        ('cat_feat',cat_pipeline,['gender','course','exam_difficulty','study_method','facility_rating']),
+        ('numeric',default_num_pipeline,['age','class_attendance'])
+        ],remainder='drop', verbose_feature_names_out=True
+)
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Stacking Regressor ? Probably not. I'll use votingregressor. Dont think my laptop can handle this one. Also catboost is leading byt a ton, so I'll weights with most going to Cat.
 
 
 
